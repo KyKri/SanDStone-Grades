55593b16476b37140c4950fd9c1b0825410a7e8e
Kyle Krick <krkrick@gmail.com>
2020-01-29 22:51:10 -0800

    updated message to include instructions

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 
 app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+    const twiml = new MessagingResponse();
 
-  twiml.message('Hello there! -Obi Wan Kenobi, Star Wars Epsidoe III');
+    twiml.message('Please include a current student id number and either ask for recent grades or current grades.');
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
 });
 
 var port = process.env.PORT || '3000';
 
 http.createServer(app).listen(port, () => {
-  console.log('Express server listening on port ' + port);
+    console.log('Express server listening on port ' + port);
 });
