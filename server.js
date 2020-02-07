df5f454bc1263626dd463eee5911f00914003c10
Kyle Krick <krkrick@gmail.com>
2020-02-06 16:51:50 -0800

    added code to process message and give instruction

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
 
-    twiml.message('Please include a current student id number and either ask for recent grades or current grades.');
+    let words = req.body.Body.split(' ');
+    let firstWord = words[0];
+    let secondWord = words[1];
+    let thirdWord = words[2];
+
+    // Make sure that that the message is of the form "[id] [current/recent] grades"
+    if (isNaN(firstWord) || !(secondWord === "recent" || secondWord ===  "current") || thirdWord !== "grades") {
+        twiml.message('Please send a messaging beginning with a student id number'
+            + ' immediately followed by either "recent grades" or "current grades".'
+            + ' For example:\n123456 recent grades');
+    } else {
+        twiml.message('Checking ' + secondWord + ' grades for ' + firstWord);
+    }
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
