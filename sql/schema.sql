c89eab3d616fe2e3c7b0074ff934afca60f1e9f1
Kyle Krick <krkrick@gmail.com>
2020-03-08 22:44:01 -0700

    added sql for db schema

---
+++ sql/schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `students` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+);
+
+CREATE TABLE `parents` (
+  `whatsapp` varchar(255) PRIMARY KEY,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+);
+
+CREATE TABLE `grade_authorizations` (
+  `whatasapp` int,
+  `student` int
+);
+
+CREATE TABLE `teachers` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+);
+
+CREATE TABLE `classes` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `teacher` int
+);
+
+CREATE TABLE `class_enrollments` (
+  `student` int,
+  `class` int
+);
+
+CREATE TABLE `subjects` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
+);
+
+CREATE TABLE `assignments` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `maxpoints` int,
+  `subject` int,
+  `class` int
+);
+
+CREATE TABLE `grades` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `student` int NOT NULL,
+  `assignment` int NOT NULL,
+  `points` int
+);
